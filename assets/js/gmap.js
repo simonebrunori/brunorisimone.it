e977d9713b5d353a33879066da13c4e25cca90f3
brunorisimone@gmail.com <brunorisimone@gmail.com>
2018-06-18 23:10:59 +0200

    Aggiuta spiegazione app distribuite e iniziata spiegazione MEAN

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
 
 		var mapOptions = {
 		// How zoomed in you want the map to start at (always required)
-		zoom: 13,
+		zoom: 16,
 		scrollwheel: false,
 
 		// The latitude and longitude to center the map (always required)
-		center: new google.maps.LatLng(41.880038, -87.601127), // Chicago
+		center: new google.maps.LatLng(43.489138, 13.177314), // Chicago
 
 		// Styling of the map in JSON object
 		styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cdd2d4"},{"visibility":"on"}]}]
@@ -25,8 +25,8 @@ $(document).ready(function(){
 		  mapOptions);
 
 		// Map marker 
-		var image = 'http://www.octarinethemes.com.s3.amazonaws.com/resources/definity/map-pin.png';
-		var myLatLng = new google.maps.LatLng(41.880038, -87.631127);
+		var image = 'assets/images/pin.png';
+		var myLatLng = new google.maps.LatLng(43.489138, 13.177314);
 		var beachMarker = new google.maps.Marker({
 			position: myLatLng,
 			map: map,
